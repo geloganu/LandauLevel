2cc08354b533fff5921fcc737fc6db467ed83eac
Logan <gelogan@uchicago.edu>
2022-08-02 12:42:56 -0500

    init landau potential builder

---
+++ qsystem/landau_potential.py
@@ -0,0 +1,11 @@
+import numpy as np
+from qsystem.constants import *
+from qsystem.particles import *
+
+class landau_potential:
+    def __init__(self):
+    
+    def build_potential(self, H):
+        
+
+    
