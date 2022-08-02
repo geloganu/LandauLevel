2f3e37b84496aaaf70de09573acd88b012cdc509
Logan <gelogan@uchicago.edu>
2022-08-01 21:02:43 -0500

    define H entries

--- hamitonian.py
+++ hamitonian.py
@@ -2,3 +2,32 @@ import numpy as np
 from constants import *
 
 #hamiltonian builder
+class hamiltonian:
+    def __init__(self, N, grid_points, V, extent, dim): 
+        '''
+        args:
+        N: number of particles
+        grid_points: number of intervals on meshgrid
+        V: potential term
+        extent: spacial extent (come back for units)
+        dim: dimensions
+        '''
+
+        self.N = N
+        self.grid_points = grid_points
+        self.V = V
+        self.dim = dim
+
+        #dx finite difference value
+        self.dx = extent/grid_points
+
+
+        #ensure N is an integer
+        if type(N) != int:
+            raise Exception('Particle number N must be of int type.')
+        
+        #ensure dimensions is correct
+        if dim in range(1,3,1):
+            raise Exception('Dimension must be either 1, 2 or 3')
+
+    
--- particles.py
+++ particles.py
@@ -3,6 +3,14 @@ from constants import *
 
 #particle builder for hamiltonian builder
 
+#single particle case N=1
 class single_particle:
-    def __init__(self, m = me):
+    def __init__(self, m = me, spin = None):
+        '''
+        m: mass of electron set to %me by default
+        spin: set to 'None' if spin polarized
+        '''
+
         self.m = m
+        self.spin = spin
+        
